86f9add6fbb0665465699366a6c21561cb5f67aa
Sylwia <sorzeszkowska@gmail.com>
2018-09-16 16:48:01 +0200

    add temp converter

--- tests/jest/api.temperature.test.js
+++ tests/jest/api.temperature.test.js
@@ -7,6 +7,17 @@ const request = supertest(url);
 describe('temperature api', () => {
   it('missing route should return 404', async () => {
     await server;
-    await request.get('/api/aaaa').expect(404);
+    await request.get('api/aaaa').expect(404);
+  });
+
+
+  it('good route should return 200', async () => {
+    await server;
+    await request.get('api/temperature').expect(200);
+  });
+
+  it('temp converter', async () => {
+    await server;
+    await request.get('api/temperature?fromValue=50&fromUnit=C&toUnit=F').expect(200);
   });
 });
